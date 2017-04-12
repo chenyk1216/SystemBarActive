20593ca9a74f25f2ab1c056cab7782c39d3f1095
chenyk <1124766087@qq.com>
2017-04-12 23:17:40 +0800

    完善sample

--- sample/src/main/java/cn/chenyk/systembarlib/MainActivity.java
+++ sample/src/main/java/cn/chenyk/systembarlib/MainActivity.java
@@ -1,16 +1,71 @@
 package cn.chenyk.systembarlib;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
-import cn.chenyk.systembarkit.manager.SystemBarManager;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    private Button btnActiveResult;
+    private RadioGroup tintTypeGroup, colorGroup;
+    private String tintTypeStr, colorStr;
+    private TextView tvAlpha;
+    private SeekBar seekbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new SystemBarManager.builder(this).setStatusBarColor(R.color.colorAccent).build();
+        seekbar = (SeekBar) findViewById(R.id.seekbar);
+        tintTypeGroup = (RadioGroup) findViewById(R.id.tintType_group);
+        colorGroup = (RadioGroup) findViewById(R.id.color_group);
+        tvAlpha = (TextView) findViewById(R.id.tv_alpha);
+        tintTypeGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                int radioButtonId = group.getCheckedRadioButtonId();
+                RadioButton rb = (RadioButton) MainActivity.this.findViewById(radioButtonId);
+                tintTypeStr = "0".equals(rb.getTag()) ? "0" : "1";
+            }
+        });
+        colorGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                int radioButtonId = group.getCheckedRadioButtonId();
+                RadioButton rb = (RadioButton) MainActivity.this.findViewById(radioButtonId);
+                colorStr = "0".equals(rb.getTag()) ? "red" : "1".equals(rb.getTag()) ? "green" : "blue";
+            }
+        });
+        ((RadioButton) tintTypeGroup.getChildAt(0)).setChecked(true);//默认选中第一个选项
+        ((RadioButton) colorGroup.getChildAt(0)).setChecked(true);//默认选中第一个选项
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                tvAlpha.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        btnActiveResult = (Button) findViewById(R.id.btn_active_result);
+        btnActiveResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ResultActivity.startCurrentActivity(MainActivity.this, tintTypeStr,
+                        tvAlpha.getText().toString(), colorStr);
+            }
+        });
     }
+
 }
---
+++ sample/src/main/java/cn/chenyk/systembarlib/ResultActivity.java
@@ -0,0 +1,54 @@
+package cn.chenyk.systembarlib;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import cn.chenyk.systembarkit.manager.SystemBarManager;
+
+/**
+ * Created by chenyk on 2017/4/12.
+ * 结果页面
+ */
+
+public class ResultActivity extends Activity {
+    public static String EXTRA_TINTTYPE_KEY = "extra_tintType_key";//色彩类型key
+    public static String EXTRA_ALPHA_KEY = "extra_alpha_key";//透明度key
+    public static String EXTRA_COLOR_KEY = "extra_Color_key";//颜色key
+
+    private SystemBarManager.TintType mTintType;
+    private int mAlpha;
+    private int mStatusBarColor;
+
+    /**
+     * 快捷启动当前页面
+     */
+    public static void startCurrentActivity(Activity activity, String tintType, String alpha,
+                                            String statusBarColor) {
+        Intent intent = new Intent(activity, ResultActivity.class);
+        intent.putExtra(EXTRA_TINTTYPE_KEY, tintType);
+        intent.putExtra(EXTRA_ALPHA_KEY, alpha);
+        intent.putExtra(EXTRA_COLOR_KEY, statusBarColor);
+        activity.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_result);
+        Intent intent = getIntent();
+        String tintTypeCode = intent.getStringExtra(EXTRA_TINTTYPE_KEY);
+        mTintType = "0".equals(tintTypeCode) ? SystemBarManager.TintType.PURECOLOR
+                : SystemBarManager.TintType.GRADIENT;
+        mAlpha = Integer.valueOf(intent.getStringExtra(EXTRA_ALPHA_KEY));
+        String colorCode = intent.getStringExtra(EXTRA_COLOR_KEY);
+        mStatusBarColor = "red".equals(colorCode) ? R.color.title_color_red :
+                "green".equals(colorCode) ? R.color.title_color_green : R.color.title_color_blue;
+        new SystemBarManager.builder(this)
+                .setStatusBarColor(mStatusBarColor)//状态栏颜色
+                .setTintType(mTintType)//色彩类型：纯色、渐变
+                .setAlpha(mAlpha)//不透明度
+                .build();
+    }
+}
